57a4bae1ee95f66116aaebdcff263453d4fa8c16
Victor Ferri <Victorferri_@hotmail.com>
2021-11-08 18:58:10 -0300

    Update JavaScriptOpenFunction

---
+++ script.js
@@ -0,0 +1,13 @@
+const Modal = {
+    open(){
+        // Open the Modal.
+        // Add Class Active in modal.
+    document
+        .querySelector('.modalOverlay')
+        .classList.add('active');
+    },
+    close(){
+        // Close the Modal.
+        // Remove the Class Active in modal.
+    }
+}
