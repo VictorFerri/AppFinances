099dab643bccbfa7f929a5c2daa8404e45db5778
Victor Ferri <Victorferri_@hotmail.com>
2021-11-08 19:01:39 -0300

    Update AddOnClickClose

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const Modal = {
     open(){
         // Open the Modal.
         // Add Class Active in modal.
-    document
-        .querySelector('.modalOverlay')
-        .classList.add('active');
+        document
+            .querySelector('.modalOverlay')
+            .classList.add('active');
     },
     close(){
         // Close the Modal.
         // Remove the Class Active in modal.
+        document
+            .querySelector('.modalOverlay')
+            .classList.remove('active');
     }
 }
